191cacd20ecfb4b0e2c51678781c7ec71f9bb405
Neetu Rai <raineetu0070@gmail.com>
2025-08-06 11:25:31 +0545

    feat:frontend routes setup and dependency installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import ChatPage from "./pages/ChatPage";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/chat" element={<ChatPage />} />
+    </Routes>
+  );
 };
 
 export default App;
