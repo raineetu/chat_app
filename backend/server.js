6f024ea3c7f83322be85db3a1f6bd1113759ca01
Neetu Rai <raineetu0070@gmail.com>
2025-08-06 11:41:35 +0545

    feat:frontend and backend connected successfully

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/api/chat", (req, res) => {
+  res.json({ message: "Hello from backend!" });
+});
+
 app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+  console.log("Server running on port 5000");
 });
--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import { useEffect } from "react";
+import axios from "axios";
 
 const ChatPage = () => {
+  const fetchChats = async () => {
+    const data = await axios.get("/api/chat");
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
   return <div>Chat page</div>;
 };
 
